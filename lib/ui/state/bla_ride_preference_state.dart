2214ae8bd4f4495c457f573547870bb4ef675f06
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2026-03-21 16:51:31 +0700

    BlA-203 - Added Ride Preference State on the Root Widget and provide method to select the current ride preference

---
+++ lib/ui/state/bla_ride_preference_state.dart
@@ -0,0 +1,46 @@
+import 'package:blabla/data/repositories/ride_preference/ride_preference_repository.dart';
+import 'package:blabla/model/ride_pref/ride_pref.dart';
+import 'package:flutter/material.dart';
+
+class BlaRidePreferenceState extends ChangeNotifier {
+  final RidePreferenceRepository ridePreferenceRepository;
+
+  RidePreference? _selectedRidePreference;
+  List<RidePreference> _ridePreferenceHistory = [];
+  bool isLoading = true;
+
+  BlaRidePreferenceState(this.ridePreferenceRepository) {
+    _init();
+  }
+
+  void _init() async {
+    loadHistory();
+  }
+
+  List<RidePreference> get ridePreferenceHistory => _ridePreferenceHistory;
+  RidePreference? get selectedRidePreference => _selectedRidePreference;
+
+  void loadHistory() async {
+    final history = await ridePreferenceRepository.fetchRidePreferences();
+    _ridePreferenceHistory = history;
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void selectPreference(RidePreference preference) {
+    if (preference != _selectedRidePreference) {
+      // Set the selected preference
+      _selectedRidePreference = preference;
+
+      // Push to history
+      _addPreferenceToHistory(preference);
+
+      notifyListeners();
+    }
+  }
+
+  void _addPreferenceToHistory(RidePreference preference) {
+    _ridePreferenceHistory.add(preference);
+    notifyListeners();
+  }
+}
